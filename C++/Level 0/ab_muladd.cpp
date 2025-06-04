f42e6a3874c2e346ffa6149f26d353299495d79c
Yu <sunny7360@naver.com>
2025-06-04 18:38:49 +0900

    Day5 두 수의 연산값 비교하기

---
+++ C++/Level 0/ab_muladd.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 아까 a + b와 b + a의 응용문제다 
+int solution(int a, int b)
+{
+    int answer = 0;
+
+    string ab = to_string(a) + to_string(b); // a + b 문자열로 치환
+    int aTob = stoi(ab);                     // 정수형으로 치환
+
+    // 만약 a + b가 2 * a * b보다 큰 경우
+    if (aTob > 2 * a * b)
+    {
+        // a + b가 됨
+        answer = aTob;
+    }
+    // 같으면 a + b 리턴하라고 했음
+    else if (aTob == 2 * a * b)
+    {
+        answer = aTob;
+    }
+    // 아니면 2 * a * b를 리턴
+    else
+    {
+        answer = 2 * a * b;
+    }
+
+    return answer;
+}
