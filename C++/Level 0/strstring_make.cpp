cbdcc90aee01bcb01676600996d5144b56112af1
Yu <sunny7360@naver.com>
2025-06-17 18:39:30 +0900

    Day12 부분 문자열 이어 붙여 문자열 만들기

---
+++ C++/Level 0/strstring_make.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> my_strings, vector<vector<int>> parts)
+{
+    string answer = "";
+
+    // Å½»ö
+    for (int i = 0; i < my_strings.size(); i++)
+    {
+        for (int j = parts[i][0]; j <= parts[i][1]; j++)
+        {
+            answer += my_strings[i][j];
+        }
+    }
+
+    return answer;
+}
